021fe0addb37633b33376f6cca7ac3b24f0e38fb
Ana Luiza <106170960+analuizataveira@users.noreply.github.com>
2025-04-08 21:33:25 -0300

    echo email destinario

--- .github/scripts/send_email.sh
+++ .github/scripts/send_email.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-sendemail -f "ana.taveiira@gmail.com" \
-          -t "$EMAIL_DESTINATARIO" \
-          -u "Notificação de Pipeline" \
-          -m "Pipeline Executado!" \
-          -s "smtp.gmail.com:587" \
-          -xu "$SMTP_USUARIO" \
-          -xp "$SMTP_SENHA" \
-          -o tls=yes
+echo "Pipeline Executado!" | mail -s "Notificação de Pipeline" "$EMAIL_DESTINATARIO"
