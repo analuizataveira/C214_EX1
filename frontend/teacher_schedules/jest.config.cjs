b1da68c2f4fad7890133545e55fbad1ef43c6fdd
analuizataveira <ana.taveira@ges.inatel.br>
2025-04-09 11:34:21 -0300

    fix: pipeline with html report

--- frontend/teacher_schedules/jest.config.cjs
+++ frontend/teacher_schedules/jest.config.cjs
@@ -1,11 +1,26 @@
-// jest.config.js
+// jest.config.cjs
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
+  moduleFileExtensions: ['js', 'json', 'ts', 'tsx'],
+  rootDir: 'src',
+  testMatch: ['**/__tests__/**/*.(test|spec).[tj]s'],
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
-  }
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  reporters: [
+    'default',
+    [
+      'jest-html-reporter',
+      {
+        outputPath: './test-report/report.html',
+        pageTitle: 'Test Report',
+      },
+    ],
+  ],
 };
