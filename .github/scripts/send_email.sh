917375d2540dce03874fb4e855d02d31c43d081b
analuizataveira <ana.taveira@ges.inatel.br>
2025-04-09 11:43:17 -0300

    testando email fixo

--- .github/scripts/send_email.sh
+++ .github/scripts/send_email.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt-get install mailutils
-echo "Pipeline Executado!" | mail -s "Notificação de Pipeline" "$EMAIL_DESTINATARIO"
+echo "Pipeline Executado!" | mail -s "Notificação de Pipeline" faelsonbr@gmail.com
